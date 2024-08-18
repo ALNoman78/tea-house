4dd0f24b41000cbf7c1846f52e8a4cde363de6e3
ALNoman78 <abdullaalno078@gmail.com>
2024-08-18 18:07:07 +0600

    banner section styles start

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+//   custom fonts added
